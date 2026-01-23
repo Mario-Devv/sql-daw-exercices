8ecb3fed199b7700d457d56539b8f944c458d1dd
Mario-Devv <marioodevv@gmail.com>
2026-01-23 11:08:34 +0100

    copy branch myExercices

---
+++ DML/1.Ejercicio/Alumno.sql
@@ -0,0 +1,8 @@
+insert into alumno (dni, nombre, apellido, fecha_nacimiento, email)
+values
+('2967839D', 'Pepe', 'Gomez', '2005-10-16', 'pepe@example.com'),
+('3927839J', 'Juan', 'López', '2001-1-16', 'juan@example.com'),
+('1934839A', 'Antonia', 'Casanova', '1982-10-1', 'antonia@example.com'),
+('2237835J', 'Ramon', 'Herrera', '2002-12-16', 'ramon@example.com');
+
+select * from alumno a;
