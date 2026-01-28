47c8a7d72b816b6703ee0c86c5274e67ce2f1da5
Mario-Devv <marioodevv@gmail.com>
2026-01-28 12:08:30 +0100

    End 8

---
+++ DML/8.Ejercicio/Ejercicio-8.sql
@@ -0,0 +1,18 @@
+/*
+ * EJERCICIO – DELETE con FOREIGN KEY
+
+Elimina de la base de datos el alumno con:
+
+dni = '3927839J'
+
+Condiciones:
+
+Usa una sola sentencia DELETE
+
+No borres directamente de matricula
+
+No modifiques la estructura de las tablas
+ * */
+
+delete from alumno
+where dni = '3927839J';
