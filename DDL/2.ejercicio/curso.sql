0c3917e68f2989c60f82576c302f165bcd936b6b
Mario-Devv <marioodevv@gmail.com>
2026-02-17 18:48:43 +0100

    clean branch

--- DDL/2.ejercicio/curso.sql
+++
@@ -1,9 +0,0 @@
-create table curso(
-	id_curso serial primary key,
-	nombre varchar(40) not null,
-	nivel integer,
-	fecha_creacion date default current_date
-	
-	constraint nivelConstraint
-	check(nivel between 1 and 4)
-)
--- myExercices/Script.sql
+++
@@ -1,6 +0,0 @@
-create table instituto(
-	id integer not null,
-	name varchar(200)
-);
-
-select * from instituto;
